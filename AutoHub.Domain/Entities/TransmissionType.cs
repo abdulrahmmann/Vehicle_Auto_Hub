e2a0dcab23dd70fa4492ae59833bfe25768d7e00
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-27 17:28:23 +0300

    Create TransmissionType Entity.

---
+++ AutoHub.Domain/Entities/TransmissionType.cs
@@ -0,0 +1,53 @@
+﻿namespace VehicleAutoHub.Domain.Entities;
+
+public class TransmissionType: Aggregate<int>
+{
+    public string Name { get; private set; } = null!;
+    
+    // public ICollection<Vehicle> VehiclesCollection { get; private set; } = new List<Vehicle>();
+
+    private TransmissionType() { }
+
+    public TransmissionType(string name)
+    {
+        Name = name;
+    }
+    
+    #region Create TransmissionType
+    public static TransmissionType CreateTransmissionType(string name)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        var transmissionType = new TransmissionType { Name = name };
+        
+        return transmissionType;
+    }
+    #endregion
+    
+    #region Update TransmissionType
+    public void UpdateTransmissionType(string? name)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        
+        Name = name ?? Name;
+    }
+    #endregion
+    
+    #region Soft Delete TransmissionType
+    public void SoftDeleteTransmissionType()
+    {
+        if (this.IsDeleted) return; // false
+        
+        this.IsDeleted = !this.IsDeleted;
+    }
+    #endregion
+    
+    #region Restore Deleted TransmissionType
+    public void RestoreDeletedTransmissionType()
+    {
+        if (!this.IsDeleted) return; // true
+        
+        this.IsDeleted = !this.IsDeleted;
+    }
+    #endregion
+}
