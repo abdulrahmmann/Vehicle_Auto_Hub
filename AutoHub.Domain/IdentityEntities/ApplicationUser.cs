37b0e1a0874a4b154dd9fd0c7859555fe709ae64
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-27 18:00:34 +0300

    Create Application User and Role.

---
+++ AutoHub.Domain/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VehicleAutoHub.Domain.IdentityEntities;
+
+public class ApplicationUser: IdentityUser<int>
+{
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiration { get; set; } 
+    
+    public bool IsDeleted { get; set; } = false;
+    public ICollection<Vehicle> VehiclesCollection { get; init; } = new List<Vehicle>();
+}
