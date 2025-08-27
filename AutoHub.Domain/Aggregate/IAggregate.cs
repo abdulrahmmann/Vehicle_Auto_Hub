ef493767198bcd83b67937592e4e222b8e1443c2
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-27 15:55:00 +0300

    Create Domian Entity, Aggregate and Domain Events.

---
+++ AutoHub.Domain/Aggregate/IAggregate.cs
@@ -0,0 +1,16 @@
+﻿using VehicleAutoHub.Domain.Common;
+using VehicleAutoHub.Domain.DomainEvents;
+
+namespace VehicleAutoHub.Domain.Aggregate;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+     
+}
+
+public interface IAggregate: IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    
+    IDomainEvent[] ClearDomainEvents();
+}
---
+++ AutoHub.Domain/Common/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleAutoHub.Domain.Common;
+
+public abstract class Entity<T>: IEntity<T>
+{
+    public T Id { get; set; }
+    
+    public DateTime? CreatedAt { get; set; }
+    
+    public string? CreatedBy { get; set; }
+    
+    public DateTime? LastModified { get; set; }
+    
+    public string? LastModifiedBy { get; set; }
+}
