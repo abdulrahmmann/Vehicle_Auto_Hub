ac34a8f36bda59d9ede929ec884dd8134964a98a
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-27 16:36:55 +0300

    Create BaseEntity Configuration.

---
+++ AutoHub.Infrastructure/Configurations/BaseEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace VehicleAutoHub.Infrastructure.Configurations;
+
+public abstract class BaseEntityConfiguration<TEntity>: IEntityTypeConfiguration<TEntity> where TEntity : Entity<int>
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(be => be.Id);
+
+        builder.Property(be => be.Id).UseIdentityColumn();
+        
+        builder.Property(e => e.CreatedAt)
+            .HasDefaultValueSql("GETUTCDATE()")
+            .IsRequired(false);
+        
+        builder.Property(be => be.CreatedBy)
+            .IsRequired(false);
+
+        builder.Property(e => e.LastModified)
+            .HasDefaultValueSql("GETUTCDATE()")
+            .IsRequired(false);
+        
+        builder.Property(be => be.LastModifiedBy)
+            .IsRequired(false);
+
+        builder.Property(be => be.IsDeleted)
+            .HasDefaultValue(false);
+    }
+}
