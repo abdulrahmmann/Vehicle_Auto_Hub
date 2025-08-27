4e1502c922a9ef31bdc1b09a0ae9a578da2e3920
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-27 18:46:23 +0300

    Registering DbContext and Identity.

--- AutoHub.Infrastructure/ModuleInfrastructure.cs
+++ AutoHub.Infrastructure/ModuleInfrastructure.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VehicleAutoHub.Infrastructure.Context;
 
 namespace VehicleAutoHub.Infrastructure;
 
@@ -7,6 +9,38 @@ public static class ModuleInfrastructure
 {
     public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services, IConfiguration configuration)
     {
+        // Register DbContext
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+        
+        // REGISTER IDENTITY
+        services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders()
+
+            .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, int>>()
+            .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, int>>();
+        
+        
+        // REGISTER UNIT OF WORK    
+        
+        // REGISTER GENERIC REPOSITORY  
+        
+        // REGISTER REPOSITORIES  
         
         return services;
     }
