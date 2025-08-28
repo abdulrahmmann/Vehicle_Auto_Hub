291cd45c828bcda4da045a935074beb076337d6e
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-28 16:02:16 +0300

    Create CQRS Abstractions.

---
+++ AutoHub.Domain/CQRS/IQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace VehicleAutoHub.Domain.CQRS;
+
+public interface IQuery<out T>: IRequest<T> where T : notnull
+{
+    
+}
---
+++ AutoHub.Domain/CQRS/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleAutoHub.Domain.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery: IQuery<TResponse>
+    where TResponse : notnull
+{
+    
+}
